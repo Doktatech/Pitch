2eb0178d89e55128026563848b157befe653bf70
Doktatech <47353946+Doktatech@users.noreply.github.com>
2019-06-14 15:56:26 +0300

    De

--- start.sh
+++
@@ -1,5 +0,0 @@
-export SECRET_KEY='GRODT'
-export MAIL_USERNAME='doktatech2@gmail.com'
-export MAIL_PASSWORD='grodtwdah2019'
-export DATABASE_URI='postgresql+psycopg2://rewel:LeBron@localhost/pitchori'
-python3.6 manage.py server
